dd5f5dbd91ec4161a1129387759d06f996c4bc94
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-12-17 03:21:20 +0200

    task_4.dart

--- task_4.dart
+++ task_4.dart
@@ -1,5 +1,5 @@
 void main() {
-  const double taxRate = 0.1;
+  const double taxRate = 0.1;   //add tax rate as constant
   var groceryList = [
     {'name': 'Milk', 'price': 46},
     {'name': 'Eggs', 'price': 100},
@@ -10,14 +10,14 @@ void main() {
     {'name': 'Rice', 'price': 27},
     {'name': 'Beans', 'price': 45},
     {'name': 'Chicken', 'price': 170},
-  ];
-  int total = 0;
-  for (var item in groceryList) {
-    print('${item['name']}: LE${item['price']}');
-    total += item['price'] as int;
+  ];  //each item is mapped to string and int values
+  int total = 0;  //intialized total variable
+  for (var item in groceryList) {  // for every item in the list 
+    print('${item['name']}: LE${item['price']}');  //print name of each item with its price
+    total += item['price'] as int;   //to calculate total price as integer
   }
-  double tax = total * taxRate;
-  double totalWithTax = total + tax;
-  print('Total: LE$total');
-  print('Total with tax: LE$totalWithTax');
+  double tax = total * taxRate;   //to caculate tax as int
+  double totalWithTax = total + tax;   //to calculate the total price after adding the tax value to it
+  print('Total: LE$total');  //print total price before tax
+  print('Total with tax: LE$totalWithTax');  //print total price with tax
 }
